15f822bc03873923b780e414ae4399b87b24382d
Artem Demidenko <ar.demidenko@appsurify.com>
2024-09-20 21:29:37 +0300

    Added support git history from SUBMODULES
    Added:
    - extra patterns
    - extra git process
    - extra parsers and submodule header cleaner

--- src/testbrain/contrib/scm/git/patterns.py
+++ src/testbrain/contrib/scm/git/patterns.py
@@ -22,19 +22,51 @@ RE_COMMIT_DIFF = re.compile(
 )
 
 
+# RE_COMMIT_LIST = re.compile(
+#     r"COMMIT:\t(?P<sha>[0-9A-Fa-f]+)\n"
+#     r"TREE:\t(?P<tree>[0-9A-Fa-f]+)\n"
+#     r"DATE:\t(?P<date>.*)\n"
+#     r"AUTHOR:\t(?P<author>.+\t.+\t.*)\n"
+#     r"COMMITTER:\t(?P<committer>.+\t.+\t.*)\n"
+#     r"MESSAGE:\t(?P<message>.*)\n"
+#     r"PARENTS:\t(?P<parents>.*)(\n{0,2})?"
+#     r"(?P<raw>(?:^:.+\n)+)?"
+#     r"(?P<numstats>(?:\d\t\d\t.+\n)+)?(\n{1,2})?"
+#     r"(?P<patch>(?:diff[ ]--git(?:.+\n?)+)+)?",
+#     re.MULTILINE | re.VERBOSE,
+# )
+# RE_COMMIT_LIST = re.compile(
+#     r"^COMMIT:\t(?P<sha>[0-9A-Fa-f]{40})\n"
+#     r"TREE:\t(?P<tree>[0-9A-Fa-f]{40})\n"
+#     r"DATE:\t(?P<date>.+)\n"
+#     r"AUTHOR:\t(?P<author>.+?\t.+?\t.+?)\n"
+#     r"COMMITTER:\t(?P<committer>.+?\t.+?\t.+?)\n"
+#     r"MESSAGE:\t(?P<message>.+)\n"
+#     r"PARENTS:\t?(?P<parents>[0-9A-Fa-f]+(?:\s[0-9A-Fa-f]+)*)?\n"
+#     r"(?P<raw>(?:^:.+\n)+)?"
+#     r"(?P<numstats>(?:\d+\t\d+\t.+\n)+)?(?:\n{1,2})?"
+#     r"(?P<patch>(?:(?!^COMMIT:).*\n?)+)?",
+#     re.MULTILINE,
+# )
 RE_COMMIT_LIST = re.compile(
-    r"COMMIT:\t(?P<sha>[0-9A-Fa-f]+)\n"
-    r"TREE:\t(?P<tree>[0-9A-Fa-f]+)\n"
-    r"DATE:\t(?P<date>.*)\n"
-    r"AUTHOR:\t(?P<author>.+\t.+\t.*)\n"
-    r"COMMITTER:\t(?P<committer>.+\t.+\t.*)\n"
-    r"MESSAGE:\t(?P<message>.*)\n"
-    r"PARENTS:\t(?P<parents>.*)(\n{0,2})?"
+    r"^COMMIT:\s(?P<sha>[0-9A-Fa-f]{40})\n"
+    r"TREE:\s(?P<tree>[0-9A-Fa-f]{40})\n"
+    r"DATE:\s(?P<date>.+)\n"
+    r"AUTHOR:\s(?P<author>.+?\t.+?\t.+?)\n"
+    r"COMMITTER:\s(?P<committer>.+?\t.+?\t.+?)\n"
+    r"MESSAGE:\s(?P<message>.+)\n"
+    r"PARENTS:\s?(?P<parents>[0-9A-Fa-f]+(?:\s[0-9A-Fa-f]+)*)?\n{1,2}"
     r"(?P<raw>(?:^:.+\n)+)?"
-    r"(?P<numstats>(?:\d\t\d\t.+\n)+)?(\n{1,2})?"
-    r"(?P<patch>(?:diff[ ]--git(?:.+\n?)+)+)?",
-    re.MULTILINE | re.VERBOSE,
+    r"(?P<numstats>(?:\d+\t\d+\t.+\n)+)?(?:\n{1,2})?"
+    r"(?P<patch>(?:(?!^COMMIT:).*\n?)+)?",
+    re.MULTILINE,
 )
 
-
 RE_REPO_NAME_PATTERN = re.compile(r".*/([^/]+)/?")
+
+RE_SUBMODULE_HEADER = re.compile(r"^Entering\s'([^']+)'\n", re.MULTILINE)
+
+RE_SUBMODULE_FILES_PATTERN = re.compile(
+    r"^Entering\s'(?P<path>[^']+)'\n(?P<files>(?:(?!^Entering).*\n?)*)",
+    re.MULTILINE,
+)
